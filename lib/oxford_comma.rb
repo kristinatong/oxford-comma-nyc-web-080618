5d4ca0d1ca0ed9c9de17c5cef07a956cd7a46aa2
Kristina Tong <kristina.tong@gmail.com>
2018-08-02 22:01:15 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.length <= 1
+    return array.join
+  elsif array.length == 2
+    return array.join(' and ')
+  else
+    new_array = []
+    for i in (0...array.length-1)
+      new_array.push(array[i])
+    end
+    new_array.push("and #{array[array.length-1]}")
+    return new_array.join(', ')
+  end
 end
